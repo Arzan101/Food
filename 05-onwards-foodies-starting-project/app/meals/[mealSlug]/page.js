cf78def411ff9e320de194f58c6b688ef296ac05
Arzan101 <azzuuu994@gmail.com>
2025-03-31 22:17:26 +0100

    main

--- 05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
+++ 05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
@@ -1,10 +1,30 @@
+import { getMeal } from '@/lib/meals'
 import classes from './page.module.css'
+import Image from 'next/image'
 
 export default function MealsDetailsPage(){
+    getMeal()
     return(
         <>
-        <header></header>
-        <main></main>
+        <header className={classes.header}>
+            <div className={classes.image}>
+                <Image fill/>
+            </div>
+            <div className={classes.headerText}>
+                <h1>Title</h1>
+                <p className={classes.creator}>
+                    by <a href = {`mailto:${'EMAIL'}`}>NAME</a>
+                </p>
+                <p className={classes.summary}>SUMMARY</p>
+            </div>
+        </header>
+        <main>
+            <p className={classes.intructions} 
+            dangerouslySetInnerHTML={{
+                __html: '...',
+                }}>
+            </p>
+        </main>
         </>
     )
 }
