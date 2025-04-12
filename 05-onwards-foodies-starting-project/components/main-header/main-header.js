b6445896afa725863f310a68012f6e2e31529fa6
Arzan101 <azzuuu994@gmail.com>
2025-04-12 15:06:11 +0100

    main

--- 05-onwards-foodies-starting-project/components/main-header/main-header.js
+++ 05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -1,23 +1,38 @@
-
+'use client';
 import Link from "next/link";
 import logoImg from '@/assets/logo.png'
-
+import { FaBars } from 'react-icons/fa';
 import classes from './main-header.module.css'
 import Image from "next/image";
+import { useState } from 'react';
 import MainHeaderBackground from "./main-header-background";
 import NavLink from "./nav-link";
 
 export default function MainHeader(){
-   
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setIsMenuOpen((prev) => !prev);
+    };
     return(
     <>
     <MainHeaderBackground/>
     <header className={classes.header}>
         <Link className={classes.logo} href="/">
         <Image src={logoImg} alt="A plate with food" priority/>
+        <span>
          NextLevel Food
+        </span>
         </Link>
-        <nav className={classes.nav}>
+
+        <button
+        className={classes.burger}
+        onClick={toggleMenu}
+        aria-label="Toggle navigation"
+      >
+        <FaBars />
+      </button>
+      <nav className={`${classes.nav} ${isMenuOpen ? classes.open : ''}`}>
             <ul>
                 <li>
                 <NavLink href="/meals">Browse Meals</NavLink>
--- 05-onwards-foodies-starting-project/components/main-header/nav-link.js
+++ 05-onwards-foodies-starting-project/components/main-header/nav-link.js
@@ -8,11 +8,12 @@ export default function NavLink({href , children}){
     const path = usePathname();
     return(
         <Link href={href} 
-        className={path.startsWith(href) 
+        className={
+            path.startsWith(href) 
         ? `${classes.link} ${classes.active}` 
         : classes.link }>
             {children}
-            Browse Meals
+            
             </Link>
     );
 }
